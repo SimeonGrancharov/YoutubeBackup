2db5ff80bcb12ebb8e5cdd6481d8fd71ef48eb7d
Simeon Grancharov <skgryncharov@gmail.com>
2023-12-06 16:24:55 +0200

    Styling of welcome screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
-import { GoogleSigninButton } from '@react-native-google-signin/google-signin'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import React, { useCallback } from 'react'
-import { Pressable, Text, View } from 'react-native'
-import { useDispatch } from 'react-redux'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import { FavouritesScreen } from './src/components/FavouritesScreen'
 import { SearchScreen } from './src/components/SearchScreen'
 import { withRedux } from './src/hoc/withRedux'
 import { useReduxAction } from './src/hooks/useReduxAction'
 import { useReduxSelector } from './src/hooks/useReduxSelector'
 import { userSlice } from './src/reducers/user'
+import { colors } from './src/constants/colors'
 
 const Tab = createBottomTabNavigator()
 
@@ -48,7 +47,22 @@ function App(): JSX.Element {
   return (
     <>
       {!isLoggedIn ? (
-        <GoogleSigninButton style={{ marginTop: 150 }} onPress={signIn} />
+        <View style={styles.loginContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.title}>YouTube Backup</Text>
+            <Text style={styles.description}>
+              This is an app for storing your favourite videos on YouTube.
+              Search for videos and add them to your favourites.
+            </Text>
+          </View>
+          <Pressable style={styles.loginButtonContainer} onPress={signIn}>
+            <Image
+              source={require('./assets/google-logo.png')}
+              style={styles.googleLogo}
+            />
+            <Text style={styles.signInText}>Sign in with Google</Text>
+          </Pressable>
+        </View>
       ) : (
         <NavigationContainer>
           <Tab.Navigator
@@ -74,5 +88,56 @@ function App(): JSX.Element {
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    backgroundColor: colors.white,
+    flex: 1,
+    padding: 30,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  title: {
+    fontSize: 32,
+    lineHeight: 32,
+    fontWeight: '500',
+    color: colors.text1
+  },
+  description: {
+    fontSize: 18,
+    lineHeight: 24,
+    textAlign: 'center',
+    marginTop: 100,
+    color: colors.text2
+  },
+  loginButtonContainer: {
+    marginBottom: 30,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: colors.text2,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 20,
+    paddingVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  googleLogo: {
+    width: 30,
+    height: 30
+  },
+  signInText: {
+    color: colors.text1,
+    marginLeft: 10,
+    fontWeight: '500',
+    fontSize: 18,
+    lineHeight: 24
+  }
+})
 
 export default withRedux(App)
